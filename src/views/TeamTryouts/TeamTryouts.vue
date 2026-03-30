a64aaa36fdd8852dc37b8139484656f2795f6ba3
zmar01 <daniel-marchant@hotmail.co.uk>
2026-03-30 13:18:21 +0100

    Center form

--- src/views/TeamTryouts/TeamTryouts.vue
+++ src/views/TeamTryouts/TeamTryouts.vue
@@ -4,12 +4,20 @@
       <h1>Team Tryouts</h1>
     </section>
     <section class="section container py-5">
-      <h3>Player Application</h3>
-      <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3fkNCnQcUkKBztPh7ERQ0EY62VzjwJ5EqHAs09Lk9iVUN0E2WjVEQVdMTVE0VkRGRkgyTkhMODNKSy4u" frameborder="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen></iframe>
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <h3>Player Application</h3>
+          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3fkNCnQcUkKBztPh7ERQ0EY62VzjwJ5EqHAs09Lk9iVUN0E2WjVEQVdMTVE0VkRGRkgyTkhMODNKSy4u" frameborder="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen></iframe>
+        </div>
+      </div>
     </section>
     <section class="section container py-5">
-      <h3>Streamer Application</h3>
-      <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3fkNCnQcUkKBztPh7ERQ0EY62VzjwJ5EqHAs09Lk9iVUQVgzVlBNMFo3MlVENFZWNDJNQVJKU09HSi4u" frameborder="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen></iframe>
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <h3>Streamer Application</h3>
+          <iframe width="640px" height="480px" src="https://forms.office.com/Pages/ResponsePage.aspx?id=3fkNCnQcUkKBztPh7ERQ0EY62VzjwJ5EqHAs09Lk9iVUQVgzVlBNMFo3MlVENFZWNDJNQVJKU09HSi4u" frameborder="0" style="border: none; max-width:100%; max-height:100vh" allowfullscreen></iframe>
+        </div>
+      </div>
     </section>
   </main>
 </template>
