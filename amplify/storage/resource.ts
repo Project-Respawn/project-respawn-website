06b1c6976951e8b4a5b684c70b8d6b01938bc480
RavensGamer <raven@ravens-gaming.com>
2026-03-31 16:10:42 +0100

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'projectRespawnStorage',
   access: (allow) => ({
-    'public/': [allow.authenticated.to(['read', 'write'])],
+    'public/*: [allow.authenticated.to(['read', 'write'])],
     'private/{entity_id}/': [allow.entity('identity').to(['read', 'write'])],
   }),
 });
