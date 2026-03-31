cc0d05bc98dc50cc6c3c12b87cc2fd4604bab3fb
zmar01 <daniel-marchant@hotmail.co.uk>
2026-03-31 13:47:41 +0100

    location of outputs.json file

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '/src/css/styles.css'
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
