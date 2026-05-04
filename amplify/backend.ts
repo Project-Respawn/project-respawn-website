cd6e807dd1c5f902bac8f6b07b72e9009c13f23e
RavensGamer <raven@ravens-gaming.com>
2026-05-04 09:17:52 +0100

    Fix import paths to use uppercase 'B' in Backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './backend/auth/resource';
-import { data } from './backend/data/resource';
-import { storage } from './backend/storage/resource';
-import { myFunction } from './backend/function/myFunction/resource';
+import { auth } from './Backend/auth/resource';
+import { data } from './Backend/data/resource';
+import { storage } from './Backend/storage/resource';
+import { myFunction } from './Backend/function/myFunction/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
