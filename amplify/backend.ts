a709e7e191593df85af30e8d809a0d3fcca5b00e
RavensGamer <raven@ravens-gaming.com>
2026-05-04 09:09:47 +0100

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
-import { storage } from './storage/resource';
-import { myFunction } from './myFunction/resource';
+import { auth } from './backend/auth/resource';
+import { data } from './backend/data/resource';
+import { storage } from './backend/storage/resource';
+import { myFunction } from './backend/function/myFunction/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
