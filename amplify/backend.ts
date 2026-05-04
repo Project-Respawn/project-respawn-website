2b8999b54705e34ffaccb12fd9608b8b10d58fea
RavensGamer <raven@ravens-gaming.com>
2026-05-04 09:24:47 +0100

    Fix import path for myFunction resource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,8 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './Backend/auth/resource';
 import { data } from './Backend/data/resource';
 import { storage } from './Backend/storage/resource';
-import { myFunction } from './Backend/function/myFunction/resource';
+import { myFunction } from './Backend/myFunction/resource';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
 defineBackend({
   auth,
   data,
